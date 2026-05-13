26950ae62a34bb1319dcf4b4c4aeae89f7178d84
Salvador HM <salvadorhm@gmail.com>
2026-05-13 14:47:45 +0000

    CREATED ejercicio hola_mundo.py

---
+++ ejercicios/semana_02/hola_mundo.py
@@ -0,0 +1,11 @@
+class NombreClase:
+
+    def __init__(self):
+        print("Constructor")
+
+    def metodoUno(self):
+        print("Método Uno")
+
+nombre_objeto = NombreClase()
+nombre_objeto.metodoUno()
+          
